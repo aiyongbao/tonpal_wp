02aedc94576b88dbc20618c48f024344a2970048
daifuyang <1140444693@qq.com>
2020-03-14 11:15:50 +0800

    新增询盘接口，增加常规注释

--- wp/files/wp-content/plugins/system-settings/library/autoload/autoload.php
+++ wp/files/wp-content/plugins/system-settings/library/autoload/autoload.php
@@ -1,15 +1,15 @@
 <?php
 
-require_once plugin_dir_path . 'route/SystemSettingsRoutes.php';
-require_once plugin_dir_path(__FILE__) . '../controller/BaseController.php';
-require_once plugin_dir_path(__FILE__) . '../controller/RestController.php';
-require_once plugin_dir_path(__FILE__) . '../Db.php';
-require_once plugin_dir_path(__FILE__) . '../middleware.php';
-require_once plugin_dir_path(__FILE__) .'../../app/http/Kernel.php';
+require_once plugin_dir_path . 'route/SystemSettingsRoutes.php'; // 加载路由
+require_once plugin_dir_path(__FILE__) . '../controller/BaseController.php'; // 系统基类
+require_once plugin_dir_path(__FILE__) . '../controller/RestController.php'; // 资源类
+require_once plugin_dir_path(__FILE__) . '../Db.php'; // 数据库类
+require_once plugin_dir_path(__FILE__) . '../middleware.php'; // 中间间类
+require_once plugin_dir_path(__FILE__) .'../../app/http/Kernel.php'; // 注册中间件
 
-require_once plugin_dir_path . '../../../wp-admin/includes/misc.php';
+require_once plugin_dir_path . '../../../wp-admin/includes/misc.php'; //wordpress内置
 
-
+//自动导入文件
 spl_autoload_register(function($class_name){
 
     $class_path = str_replace('\\','/',$class_name);
