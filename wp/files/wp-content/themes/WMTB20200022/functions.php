7d811298c6fe264fbf48138b772d9d597c4cd235
sky8196 <1316687982@qq.com>
2020-04-11 15:41:59 +0800

    修改首页json读取逻辑

--- wp/files/wp-content/themes/WMTB20200022/functions.php
+++ wp/files/wp-content/themes/WMTB20200022/functions.php
@@ -45,8 +45,8 @@ if (!function_exists('my_theme_setup')) :
                             }
                             $filename = $parentDir. '/' .$filename;
                             global $wpdb;
-                            $wpdb->theme_file = "wp_theme_file";
-                            $result = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->theme_file WHERE file = %s",$filename ) );
+                            $abbr = get_query_var('lang') ? get_query_var('lang'). '_' : '';
+                            $result = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}{$abbr}theme_file WHERE file = %s ",$filename ) );
                             $data = [
                                 'is_public' =>  strpos($item['action'],'public') !==false  ? 1 : 0,
                                 'theme' => wp_get_theme()->get('Name'),
@@ -198,8 +198,9 @@ function json_config_array($file,$type = 'vars',$public = 0)
 
 
     global $wpdb;
-    $wpdb->theme_file = "wp_theme_file";
-    $result = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->theme_file WHERE file = %s AND is_public = %d LIMIT 1", $file, $public ) );
+    $abbr = get_query_var('lang') ? get_query_var('lang'). '_' : '';
+    $result = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}{$abbr}theme_file WHERE file = %s AND is_public = %d LIMIT 1",$file, $public ) );
+
 
     if($result !== false)
     {
