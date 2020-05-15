b32ab37b0838a6a9210751a27061e8f3a176882a
daifuyang <1140444693@qq.com>
2020-05-15 15:47:29 +0800

    修复导航链接，新增隐私条款json，新增清空缓存接口

--- wp/files/wp-content/plugins/system-settings/route/SystemSettingsRoutes.php
+++ wp/files/wp-content/plugins/system-settings/route/SystemSettingsRoutes.php
@@ -331,6 +331,15 @@ class SystemSettingsRoutes {
             }
         ));
 
+        //清理nginx缓存
+        register_rest_route($this->namespace, '/clear' ,array(
+            'methods'  => WP_REST_Server::READABLE,
+            'callback' => function($request){
+                $settings = new SettingController();
+                return middleware::run('api')->init( $settings->clear($request),$request);
+            }
+        ));
+
     }
 }
 
